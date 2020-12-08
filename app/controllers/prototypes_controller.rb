6c4560de38e220348f3f3405d2306724a65a8602
mitsugu-kawaguchi <74910089+mitsugu-kawaguchi@users.noreply.github.com>
2020-12-08 14:57:34 +0900

    未ログイン時一覧表示に修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:show,:index,:new]
+  before_action :authenticate_user!, except: [:show, :index]
   before_action :set_prototype, only: [:edit, :show]
 
   def index
